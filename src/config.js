bd2b891a00ddf3d5089813a1ce9c32147df089ae
Nikhil Vijayan <2021600+nkhil@users.noreply.github.com>
2021-02-07 12:32:14 +0000

    feat: change city to Edinburgh temporarily

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   port: process.env.PORT || 8080,
-  city: 'London',
+  city: 'Edinburgh',
   openWeather: {
     apiKey: process.env.OPENWEATHER_API_KEY,
     URI: 'https://api.openweathermap.org/data/2.5/weather',

55dae4cf654f0dfa18a084be32b1b36317c27c1c
Nikhil Vijayan <2021600+nkhil@users.noreply.github.com>
2021-02-13 16:32:47 +0000

    test: :white_check_mark: add tests to capitalise string

--- src/utils/capitalise.js
+++ src/utils/capitalise.js
@@ -1,5 +1,7 @@
 
 function capitalise(string) {
+	if (string === '') return ''
+	if (!string) throw new Error('Invalid parameter provided to capitalize')
 	return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,12 +1,21 @@
-const { convertKelvinToCelsius } = require('../src/utils/temperature');
+const { convertKelvinToCelsius } = require('../src/utils/temperature')
+const { capitalise } = require('../src/utils/capitalise')
 
 describe('Utils', () => {
-  describe('convertKelvinToCelsius', () => {
+  describe('#convertKelvinToCelsius', () => {
     it('Can convert Kelvin to Celcius', () => {
       expect(convertKelvinToCelsius(23)).toBe("-250.1")
       expect(convertKelvinToCelsius(50)).toBe("-223.1")
       expect(convertKelvinToCelsius(100)).toBe("-173.1")
       expect(convertKelvinToCelsius(300)).toBe("26.9")
+    })
+  })
+
+  describe('#capitalise', () => {
+    it('Can capitalise the first character of a given string', () => {
+      expect(capitalise('hello')).toBe('Hello')
+      expect(capitalise('')).toBe('')
+      expect(() => capitalise(undefined)).toThrow('Invalid parameter provided to capitalize')
     })
   })
 })

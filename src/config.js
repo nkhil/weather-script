1950effe613edd7b8b3b7da19f3874c619dea690
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-02-26 19:01:37 +0000

    feat: ✨ Add linting

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-  port: process.env.PORT || 8080,
-  city: 'London',
-  openWeather: {
-    apiKey: process.env.OPENWEATHER_API_KEY,
-    URI: 'https://api.openweathermap.org/data/2.5/weather',
-  },
+	port: process.env.PORT || 8080,
+	city: 'London',
+	openWeather: {
+		apiKey: process.env.OPENWEATHER_API_KEY,
+		URI: 'https://api.openweathermap.org/data/2.5/weather',
+	},
 };
